d9da914363827bf2f34a0902b3d6e3441ef95c79
ducong <duconggpdg@gmail.com>
2022-04-12 23:38:39 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hi!')
+})
+
+app.listen(3000, () => console.log('Server ready'))
